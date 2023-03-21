726a0d6f78929dc0bf2d32e9ecc6ecaa7aac6df6
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-03-21 20:31:08 +0900

    Init status

---
+++ generateDockerCompose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CHAIN_ID=1000
+SCHAIN_ID=2000
+NETWORK_ID=203
+NUM_CNS=1
+NUM_PNS=0
+NUM_ENS=0
+NUM_SCNS=0
+
+# Download klaytn docker image.
+docker pull klaytn/klaytn:latest
+
+# Generate docker-compose.yml for docker-compose.
+docker run --rm -v ${PWD}:/homi-output klaytn/klaytn homi setup -o /homi-output \
+	--docker-image-id klaytn/klaytn:latest \
+	--chainID $CHAIN_ID --cn-num $NUM_CNS  --pn-num $NUM_PNS --en-num $NUM_ENS \
+	--servicechain --serviceChainID $SCHAIN_ID --scn-num $NUM_SCNS \
+	--network-id $NETWORK_ID docker
